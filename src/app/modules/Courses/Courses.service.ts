ec93887c6500964045db9325b7ba17d81f76e8a8
sarwar-asik <sarwarasik@gmail.com>
2023-11-08 21:07:39 +0600

    course service

--- src/app/modules/Courses/Courses.service.ts
+++ src/app/modules/Courses/Courses.service.ts
@@ -1,13 +1,17 @@
-
-import {  Prisma,Courses } from '@prisma/client';
+import { Courses } from '@prisma/client';
 import prisma from '../../../shared/prisma';
 
 const insertDB = async (data: Courses): Promise<Courses> => {
-  const result = await prisma.Courses.create({
+  const result = await prisma.courses.create({
     data,
   });
 
   return result;
 };
 
-export const CoursesService = {insertDB};
+const getAllDb = async () :Promise<Courses[]>=> {
+  const result = await prisma.courses.findMany();
+
+  return result;
+};
+export const CoursesService = { insertDB ,getAllDb};
