a58a08ca46ed739be68bf0e725015be95feb9302
sarwar-asik <sarwarasik@gmail.com>
2023-11-08 21:09:15 +0600

    course controller done

--- src/app/modules/Courses/Courses.controller.ts
+++ src/app/modules/Courses/Courses.controller.ts
@@ -3,6 +3,8 @@ import { Request, Response } from "express";
 import catchAsync from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sendResponse";
 import httpStatus from "http-status";
+import { CoursesService } from "./Courses.service";
+import { Courses } from "@prisma/client";
 
 const insertDB = catchAsync(async (req: Request, res: Response) => {
   const data = req.body;
@@ -11,9 +13,20 @@ const insertDB = catchAsync(async (req: Request, res: Response) => {
   sendResponse<Courses>(res, {
     statusCode: httpStatus.CREATED,
     success: true,
-    message: 'Successfully Courses',
+    message: 'Successfully create Courses',
+    data: result,
+  });
+});
+const getAllDb = catchAsync(async (req: Request, res: Response) => {
+
+  const result = await CoursesService.getAllDb()
+
+  sendResponse<Courses[]>(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: 'Successfully fetched Courses',
     data: result,
   });
 });
 
-export const CoursesController = {insertDB};
+export const CoursesController = {insertDB,getAllDb};
